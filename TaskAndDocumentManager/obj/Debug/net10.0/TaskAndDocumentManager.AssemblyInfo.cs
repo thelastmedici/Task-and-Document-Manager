97b15e23f1896ba46fedffabede4d04f0061bb3e
thelastmedici <christianopeyemijoshua@gmail.com>
2026-01-31 08:36:05 +0000

    feat: declared insterfaces

--- TaskAndDocumentManager/obj/Debug/net10.0/TaskAndDocumentManager.AssemblyInfo.cs
+++ TaskAndDocumentManager/obj/Debug/net10.0/TaskAndDocumentManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskAndDocumentManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2808dcd302ba6cf8268523f358cd4ae6001fd539")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+455c5ac414d99d09ec7909975a874bc5e2409729")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskAndDocumentManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskAndDocumentManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
