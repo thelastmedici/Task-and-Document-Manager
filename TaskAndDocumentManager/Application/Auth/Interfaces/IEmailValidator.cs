ebb525720a7877a97a6e8705617ff2bcc7b215eb
thelastmedici <christianopeyemijoshua@gmail.com>
2026-02-07 12:04:43 +0000

    added interface for email validation

--- TaskAndDocumentManager/Application/Auth/Interfaces/IEmailValidator.cs
+++ TaskAndDocumentManager/Application/Auth/Interfaces/IEmailValidator.cs
@@ -1,5 +1,8 @@
 //email validation contract interface for usecase lives here
 namespace TaskAndDocumentManager.Apploication.Auth.interfaces
 {
-    
+    public interface IEmailValidator
+    {
+        bool IsValid(string email);
+    }
 }
