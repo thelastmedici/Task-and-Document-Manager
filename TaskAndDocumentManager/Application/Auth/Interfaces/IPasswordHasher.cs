a01282a5848b99707e518bf81e9140bb0b74dfc8
thelastmedici <christianopeyemijoshua@gmail.com>
2026-02-08 19:31:42 +0000

    feat: added the password asher interface

--- TaskAndDocumentManager/Application/Auth/Interfaces/IPasswordHasher.cs
+++ TaskAndDocumentManager/Application/Auth/Interfaces/IPasswordHasher.cs
@@ -1,1 +1,9 @@
-interface
+//THis interface holds the contract for password hasing and verification
+namespace TaskAndDocumentManager.Application.Auth.interfaces
+{
+    public interface IPasswordHasher
+    {
+         string Hash(string password);
+         bool VerifyPassword(string password);
+    }
+}
