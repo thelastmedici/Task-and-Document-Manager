7c276af3d32ee0e76453729193bbdd0608469a09
thelastmedici <christianopeyemijoshua@gmail.com>
2026-02-10 16:06:26 +0000

    pushed changes

--- TaskAndDocumentManager/obj/Debug/net10.0/TaskAndDocumentManager.AssemblyInfo.cs
+++ TaskAndDocumentManager/obj/Debug/net10.0/TaskAndDocumentManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskAndDocumentManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e8a84c943cf558b13c2887861c7220ff8153b2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8a80ca5525ba2b879ba2a530452dd675474c334")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskAndDocumentManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskAndDocumentManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
