53feb515a7b3a26af5d0ea64c2bf73a10944e4c4
thelastmedici <christianopeyemijoshua@gmail.com>
2026-02-11 12:51:54 +0000

    added private readonly fields containing interfaces

--- TaskAndDocumentManager/Application/Auth/UseCases/RegisterUser.cs
+++ TaskAndDocumentManager/Application/Auth/UseCases/RegisterUser.cs
@@ -1,36 +1,15 @@
 using System;
 using System.Linq;
 using System.Net.Mail;
+using TaskAndDocumentManager.Application.Auth.Interfaces;
 
 namespace TaskAndDocumentManager.Application.Auth.UseCases
+{
+    public class RegisterUser
     {
-        public class RegisterUser
-        {
+        private readonly IUserRepository _userRepository;
+        private readonly IPasswordHasher _passwordHasher;
+        private readonly IEmailValidator _emailValidator;
 
-            //Implementation of register user usecases
-
-            //1)Email validation
-            
-
-//                 Later:
-
-// Extract IEmailValidator
-
-// Move implementation out
-
-// Plug it back in
-
-        
-                //TODO Password validation c#
-                public bool IsPasswordStrong(string password)
-                {    
-                            if (string.IsNullOrWhiteSpace(password))
-                            {
-                                return false;
-                            }
-                            return password.Length >= 8 && password.Any(char.IsDigit) && password.Any(char.IsUpper);
-                    
-                }
-        }
-        
     }
+}
