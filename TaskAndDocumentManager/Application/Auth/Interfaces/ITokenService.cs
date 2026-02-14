36d8a4304948c9ae99be3b030a36e4b69a0dd250
thelastmedici <christianopeyemijoshua@gmail.com>
2026-02-14 11:42:34 +0000

    added Itoken

--- TaskAndDocumentManager/Application/Auth/Interfaces/ITokenService.cs
+++ TaskAndDocumentManager/Application/Auth/Interfaces/ITokenService.cs
@@ -1,1 +1,10 @@
-// this is for itoken service
+using System;
+
+namespace TaskAndDocumentManager.Application.Auth.Interfaces;
+
+public interface ITokenService
+{
+    string GenerateToken(string userId, string email, string role);
+    bool ValidateToken(string token);
+    string GetUserIdFromToken(string token);
+}
