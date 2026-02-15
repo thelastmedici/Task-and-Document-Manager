72daa208e5b193aaec4b0fe1bafa27544fc301f3
thelastmedici <christianopeyemijoshua@gmail.com>
2026-02-15 14:25:08 +0000

    chore:added ithoekn interface

--- TaskAndDocumentManager/Application/Auth/Interfaces/ITokenService.cs
+++ TaskAndDocumentManager/Application/Auth/Interfaces/ITokenService.cs
@@ -8,3 +8,25 @@ public interface ITokenService
     bool ValidateToken(string token);
     string GetUserIdFromToken(string token);
 }
+
+using System;
+
+namespace TaskAndDocumentManager.Application.Auth.Interfaces;
+
+public interface ITokenService
+{
+    string GenerateToken(string userId, string email, string role);
+    bool ValidateToken(string token);
+    string GetUserIdFromToken(string token);
+}
+
+using System;
+
+namespace TaskAndDocumentManager.Application.Auth.Interfaces;
+
+public interface ITokenService
+{
+    string GenerateToken(string userId, string email, string role);
+    bool ValidateToken(string token);
+    string GetUserIdFromToken(string token);
+}
--- TaskAndDocumentManager/obj/Debug/net10.0/TaskAndDocumentManager.AssemblyInfo.cs
+++ TaskAndDocumentManager/obj/Debug/net10.0/TaskAndDocumentManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskAndDocumentManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8a80ca5525ba2b879ba2a530452dd675474c334")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36d8a4304948c9ae99be3b030a36e4b69a0dd250")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskAndDocumentManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskAndDocumentManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
