a02458fb8ab9a724059d95adc1f52435cd3e6c54
thelastmedici <christianopeyemijoshua@gmail.com>
2026-03-01 22:12:44 +0000

    added user validation logic

--- TaskAndDocumentManager/Application/Auth/UseCases/RegisterUser.cs
+++ TaskAndDocumentManager/Application/Auth/UseCases/RegisterUser.cs
@@ -7,18 +7,39 @@ namespace TaskAndDocumentManager.Application.Auth.UseCases
 {
     public class RegisterUser
     {
+         
+          //A constructor that accept IUserRepository IPasswordHasher IEmailValidator and assigns them to the private field
+         public RegisterUser(IUserRepository userRepository, IPasswordHasher passwordHasher, IEmailValidator emailValidator, IPasswordValidator passwordValidator){
+            _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
+            _emailValidator = emailValidator;
+            _passwordValidator = passwordValidator;
+        }
+
         private readonly IUserRepository _userRepository;
         private readonly IPasswordHasher _passwordHasher;
         private readonly IEmailValidator _emailValidator;
 
+        private readonly IPasswordValidator _passwordValidator;
 
-        
-        //A constructor that accept IUserRepository IPasswordHasher IEmailValidator and assigns them to the private field
-        public RegisterUser(IUserRepository userRepository, IPasswordHasher passwordHasher, IEmailValidator emailValidator){
-            _userRepository = userRepository;
-            _passwordHasher = passwordHasher;
-            _emailValidator = emailValidator;
+
+    
+        public void Execute(string email, string password)
+        {
+            if(!_emailValidator.IsValidEmail(email))
+            {
+                throw new FormatException("Email is invalid");
+            }
+
+            var existingUser = _userRepository.GetByEmail(email);
+
+            if(existingUser != null)
+            {
+                throw new InvalidOperationException("User Already Exist");
+            }
         }
+       
+       
 
     }
 }
