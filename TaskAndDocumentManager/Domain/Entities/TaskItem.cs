01c9d864ec7285b65e462bebf0be690dcef4a2c9
thelastmedici <christianopeyemijoshua@gmail.com>
2026-03-16 14:42:34 +0000

    chore: taskitem properties

---
+++ TaskAndDocumentManager/Domain/Entities/TaskItem.cs
@@ -0,0 +1,11 @@
+namespace TaskAndDocumentManager.Domain.Entities;
+
+class TaskItem
+{
+    private int id; // Unique Identifier
+    private string Title; // Name of Task
+
+    private string Description; // Details for the task
+
+    private DateTime CreatedAt;
+}
