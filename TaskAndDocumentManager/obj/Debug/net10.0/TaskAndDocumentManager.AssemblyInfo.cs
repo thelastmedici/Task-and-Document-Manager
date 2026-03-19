291949840266f06c0b21583a9b0c473488bb7109
thelastmedici <christianopeyemijoshua@gmail.com>
2026-03-19 13:28:04 +0000

    feat: added constructor for taskItem object creation

--- TaskAndDocumentManager/obj/Debug/net10.0/TaskAndDocumentManager.AssemblyInfo.cs
+++ TaskAndDocumentManager/obj/Debug/net10.0/TaskAndDocumentManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskAndDocumentManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acce0f7280daf4758db3758f13a054fc584660c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+758f28413261ba89f418ae7695bedfb6ad887b84")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskAndDocumentManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskAndDocumentManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
