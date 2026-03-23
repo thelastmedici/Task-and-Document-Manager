bc1febeaf70215f88a4116250ce60f343605bc88
thelastmedici <christianopeyemijoshua@gmail.com>
2026-03-23 13:06:18 +0000

    rewrote task repository

--- TaskAndDocumentManager/Application/TodoTask/Interfaces/ITaskRepository.cs
+++ TaskAndDocumentManager/Application/TodoTask/Interfaces/ITaskRepository.cs
@@ -1,22 +1,21 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using TaskAndDocumentManager.Domain.Entities;
 
-namespace Application.TodoTask.Interfaces
+namespace TaskAndDocumentManager.Application.Tasks.Interfaces
 {
-   public interface ITaskRepository
-{
-   Task<TaskItem> CreateAsync(TaskItem task, CancellationToken cancellationToken=default);// method is asynchronous and returns a TaskItem when finished
+    public interface ITaskRepository
+    {
+        Task CreateAsync(TaskItem task, CancellationToken cancellationToken = default);
 
-   Task<TaskItem?> GetByIdAsync(Guid id, CancellationToken cancellationToken=default);
+        Task<TaskItem?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
 
-   Task<IReadOnlyList<TaskItem>> GetAllAsync(CancellationToken cancellationToken=default);
+        Task<IReadOnlyList<TaskItem>> GetAllAsync(CancellationToken cancellationToken = default);
 
-   Task UpdateAsync(TaskItem task, CancellationToken cancellationToken=default); // update existing task
+        Task UpdateAsync(TaskItem task, CancellationToken cancellationToken = default);
 
-   Task DeleteAsync(Guid id, CancellationToken cancellationToken=default); // delete completed task
-}
-
+        Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+    }
 }
