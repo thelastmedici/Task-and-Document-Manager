d2ef6af3892147286ae3026baaf0d51917a243c8
thelastmedici <christianopeyemijoshua@gmail.com>
2026-04-09 10:27:00 +0100

    feat: configured Role entity in TaskDBContext.. seed admin and Manager role records

--- TaskAndDocumentManager/Infrastructure/Tasks/TaskDbContext.cs
+++ TaskAndDocumentManager/Infrastructure/Tasks/TaskDbContext.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using TaskAndDocumentManager.Domain.Auth;
+using TaskAndDocumentManager.Domain.Entities;
 using TaskAndDocumentManager.Domain.Tasks;
 
 namespace TaskAndDocumentManager.Infrastructure.Tasks;
@@ -21,5 +23,35 @@ public class TaskDbContext(DbContextOptions<TaskDbContext> options) : DbContext(
             entity.Property(task => task.CreatedAt)
                 .IsRequired();
         });
+
+        modelBuilder.Entity<Role>(entity =>
+        {
+            entity.HasKey(role => role.Id);
+
+            entity.Property(role => role.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+
+            entity.HasIndex(role => role.Name).IsUnique();
+
+           entity.HasData(
+            new Role
+            {
+                Id = BuiltInRoles.AdminId,
+                Name = BuiltInRoles.AdminName,
+            },
+
+            new Role
+            {
+                Id = BuiltInRoles.ManagerId,
+                Name = BuiltInRoles.ManagerName
+            }
+           ); 
+        });
+
+        // modelBuilder.Entity<User>(entity =>
+        // {
+            
+        // })
     }
 }
